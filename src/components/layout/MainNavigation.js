24e273bac0dbebd6e58f689f7d20eeb611a5e73b
James Beswick <james@192.168.1.173>
2021-09-22 16:20:56 +0100

    Added navbar

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import classes from './MainNavigation.module.css';
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link>Home</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
