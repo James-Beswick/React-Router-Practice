d8a17b5702291447575f1793d80314673a6385e4
James Beswick <james@192.168.1.173>
2021-09-22 16:13:48 +0100

    added a quote api

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,24 +1,51 @@
+import { useState } from 'react';
 import QuoteList from '../components/quotes/QuoteList';
 import Card from '../components/UI/Card';
+import classes from './AllQuotes.module.css';
 
-const quoteList = [
+const dummy = [
   {
-    id: 'q1',
-    author: 'James',
-    text: 'This is just a wee bit of placeholder text, nothing to see here :)',
+    id: 1,
+    text: 'yada yada yada',
+    author: 'me',
   },
-  {
-    id: 'q3',
-    author: 'Semaj',
-    text: "But where did quote two go? I don't know -\\_:)_/-",
-  },
-]; //dummy data for the mo.
+];
 
-const AllQuotes = props => {
+const AllQuotes = () => {
+  const [quotes, setQuotes] = useState([]);
+
+  const fetchQuotesHandler = async () => {
+    try {
+      const res = await fetch('https://type.fit/api/quotes/?_limit=10');
+
+      if (!res.ok) {
+        throw new Error('Something Went Wrong!');
+      }
+
+      const data = await res.json();
+      const quoteList = [];
+
+      for (const key in data) {
+        quoteList.push({
+          id: key,
+          text: data[key].text,
+          author: data[key].author,
+        });
+      }
+
+      setQuotes(quoteList);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Card>
-      <h1>All Quotes</h1>
-      <QuoteList quotes={quoteList} />
+      <span className={classes.h1ButtonSpan}>
+        <h1>All Quotes</h1>
+        <button onClick={fetchQuotesHandler}>Generate Quotes</button>
+      </span>
+      <QuoteList quotes={quotes} />
     </Card>
   );
 };
