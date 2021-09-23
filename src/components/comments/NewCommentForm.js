c7cc0d89c7884af7ab19895eee71fbf938b8a958
James Beswick <james@192.168.1.173>
2021-09-23 16:30:25 +0100

    implemented newcomment functionality

--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,26 +1,58 @@
 import { useRef } from 'react';
+import { useParams } from 'react-router-dom';
 
 import classes from './NewCommentForm.module.css';
 
-const NewCommentForm = (props) => {
+const NewCommentForm = props => {
   const commentTextRef = useRef();
+  const params = useParams();
+  const id = params.id;
 
-  const submitFormHandler = (event) => {
+  const postCommentHandler = async enteredComment => {
+    try {
+      const newComment = {
+        ...enteredComment,
+      };
+      const res = await fetch(
+        `https://user-comments-project-default-rtdb.europe-west1.firebasedatabase.app/${id}.json`,
+        {
+          method: 'POST',
+          body: JSON.stringify(newComment),
+          headers: {
+            'Content-Type:': 'application/json',
+          },
+        }
+      );
+
+      if (!res.ok) {
+        throw new Error('Posting Comment To Database Failed!');
+      }
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const submitFormHandler = event => {
     event.preventDefault();
 
     // optional: Could validate here
+    const enteredComment = {
+      id: Math.random(),
+      text: commentTextRef,
+    };
 
-    // send comment to server
+    postCommentHandler(enteredComment);
   };
 
+  // see if I need to change the second onSubmit handler
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
